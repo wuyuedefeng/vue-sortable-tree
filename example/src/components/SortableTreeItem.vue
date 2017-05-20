1630b1912bcf0ad6addf8dae0d9bb9d219f83ecd
mars <578595193@qq.com>
2017-05-20 10:50:14 +0800

    fix

--- example/src/components/SortableTreeItem.vue
+++ example/src/components/SortableTreeItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="sortable-tree-item-drag" :draggable="item[attr]" @dragstart.stop="dragStart($event)" @dragover.prevent @dragenter.stop="dragEnter($event)"
-       @dragleave.stop="dragLeave(item, $event)" @drop.stop="drop" @dragend.stop.prevent="dragEnd" :class="{draging: isDraging}">
+  <div class="sortable-tree-item-drag" :draggable="item[attr]" @dragstart.stop="dragStart()" @dragover.prevent @dragenter.stop="dragEnter()"
+       @dragleave.stop="dragLeave(item)" @drop.stop="drop" @dragend.stop.prevent="dragEnd">
     <slot :item="item"></slot>
   </div>
 </template>
@@ -15,9 +15,6 @@ export default {
     }
   },
   computed: {
-    isDraging () {
-      return this.dragObj.data === this.item
-    }
   },
   methods: {
     calcStatus () {
@@ -25,28 +22,24 @@ export default {
       let equalParent = null
       let equalItemChild = null
       let equalDragChild = null
-      let tItemParent = tItem.$parent
-      console.log('000', tItemParent.name)
-      while (tItemParent) {
+      while (tItem.$parent) {
         let tDrag = this.dragObj.data
-        let tDragParent = tDrag.$parent
-        while (tDragParent) {
-          if (tItemParent === tDragParent) {
-            equalParent = tItemParent
+        while (tDrag.$parent) {
+          if (tItem.$parent === tDrag.$parent) {
+            equalParent = tItem.$parent
             equalItemChild = tItem
             equalDragChild = tDrag
             break
           } else {
-            tDragParent = tDragParent.$parent
+            tDrag = tDrag.$parent
           }
         }
         if (equalParent) {
           break
         } else {
-          tItemParent = tItemParent.$parent
+          tItem = tItem.$parent
         }
       }
-      console.log(1111, equalParent.name)
       return {
         isToFront: equalParent.children.indexOf(equalItemChild) <= equalParent.children.indexOf(equalDragChild),
         equalParent,
@@ -54,39 +47,21 @@ export default {
         equalDragChild
       }
     },
-    dragStart (event) { // 被拖动元素
-      this.dragObj.vm = event.target
+    dragStart () { // 被拖动元素
+      this.dragObj.vm = this.$el
       this.dragObj.data = this.item
     },
-    dragEnter (event) { // 作用在目标元素
+    dragEnter () { // 作用在目标元素
       this.dragObj.vm.classList.add('draging')
-      if (this.dragObj.calculating) return
-      this.dragObj.calculating = true
       if (this.dragObj.data !== this.item) {
-        console.log(this.dragObj.vm, event.target)
-        let index = this.dragObj.data.$parent.children.indexOf(this.dragObj.data)
         let status = this.calcStatus()
-        this.dragObj.data.$parent.children.splice(index, 1)
-        this.dragObj.data.$parent = status.equalParent
-        let itemIndex = status.equalParent.children.indexOf(status.equalItemChild)
-        if (status.isToFront) {
-          status.equalParent.children.splice(itemIndex, 0, status.equalDragChild)
-        } else {
-          if (itemIndex === status.equalParent.children.length - 1) {
-            status.equalParent.children.push(status.equalDragChild)
-          } else {
-            status.equalParent.children.splice(itemIndex + 1, 0, status.equalDragChild)
-          }
-        }
+        console.log(this.dragObj.data, this.item, status.isToFront, status)
       }
-      setTimeout(() => {
-        this.dragObj.calculating = false
-      }, 500)
     },
-    dragLeave (data, event) { // 作用在目标元素
+    dragLeave (data) { // 作用在目标元素
     },
     // 在ondragover中一定要执行preventDefault()，否则ondrop事件不会被触发。
-    drop (event) { // 目标元素
+    drop () { // 目标元素
       this.dragObj.vm.classList.remove('draging')
     },
     dragEnd () {
